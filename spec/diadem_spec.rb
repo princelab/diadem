2852e69e131aee497a2bb3f984d7b35ca7d64ec2
John Prince <jtprince@gmail.com>
2013-08-15 17:43:33 -0600

    basic code working

--- spec/diadem_spec.rb
+++ spec/diadem_spec.rb
@@ -8,23 +8,32 @@ describe 'calculating enrichement' do
     element = :H
     mass_number = 2
     aaseq = 'AAI'
+    #aaseq = 'ELENLAAMDLELQK'
     calc = Diadem::Calculator.new(element, mass_number)
-    enrichments = (0..1).step(0.1).to_a.reverse.map {|v| v.round(1) }
+    enrichments = (0..1).step(0.1).map {|v| v.round(1) }
     spectra = calc.plot_enrichments(aaseq, enrichments)
+
+    max_penetration = calc.max_penetration_fraction(aaseq, Mspire::MolecularFormula.from_aaseq(aaseq)).round(2)
+
+    lo_x = spectra.last.mzs.first.floor
+    hi_x = spectra.last.mzs.last.ceil
+    hi_y = spectra.first.intensities.first
 
     spectra.zip(enrichments) do |spectrum, enrich_frac|
 
-      base = "enrichment_#{enrich_frac}"
+      base = "#{aaseq}_enrichment_#{enrich_frac}"
       outfile = base + ".dat"
+      svgfile = base + ".svg"
       File.open(outfile, 'w') do |gp|
         Gnuplot::Plot.new(gp) do |plot|
 
-
-          plot.title [aaseq, "element: #{element}", "mass_num: #{mass_number}", "enrich_frac: #{enrich_frac}", "max_penetration: ", calc.max_penetration_fraction(aaseq, Mspire::MolecularFormula.from_aaseq(aaseq)) ].join(" ")
+          plot.title [aaseq, "el:#{element}", "massnum:#{mass_number}", "enrich:#{enrich_frac}", "max_penetration:#{max_penetration}",  ].join(", ")
           plot.terminal "svg"
-          plot.output base + ".svg"
-          plot.xrange "[273:290]"
-          plot.yrange "[0:0.9]"
+          plot.output svgfile
+          plot.xrange "[#{lo_x}:#{hi_x}]"
+          plot.yrange "[0:#{hi_y}]"
+          plot.xlabel "mass"
+          plot.ylabel "relative intensity"
 
           plot.data = [
             Gnuplot::DataSet.new( spectrum.data_arrays ) {|ds| ds.with = "impulses" }
@@ -32,7 +41,9 @@ describe 'calculating enrichement' do
         end
       end
       system "gnuplot #{outfile}"
+      system "svg_to_pxx.rb -b white -p -d 90 #{svgfile}"
       File.unlink(outfile)
+      File.unlink(svgfile)
     end
   end
 end
